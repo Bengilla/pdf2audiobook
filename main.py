8a810a29508f2167ff8fa31d134bc8a67fc3ddb6
Bengilla <bengilla@outlook.com>
2022-01-05 20:15:46 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import pyttsx3
 import PyPDF2
-import os
+
+book = open("object_oriented_python_tutorial.pdf", "rb")
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+
+speaker = pyttsx3.init()
+for num in range(1, pages):
+    page = pdfReader.getPage(7)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
